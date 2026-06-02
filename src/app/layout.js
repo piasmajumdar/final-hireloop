ae5decbbe15cab7f0cdbc1e3f9be00eb07f26654
piasmajumdar <piasmajumdar@gmail.com>
2026-06-02 10:30:41 +0530

    Navbar layout set up

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/componnents/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +24,12 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} dark h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Navbar></Navbar>
+        <main>
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
