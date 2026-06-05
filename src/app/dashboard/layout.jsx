554dcaac7a09d50e6e119389364668d3b453cb8b
piasmajumdar <piasmajumdar@gmail.com>
2026-06-05 16:56:49 +0530

    dashboard page created

---
+++ src/app/dashboard/layout.jsx
@@ -0,0 +1,10 @@
+
+const DashBoardLayout = ({ children }) => {
+    return (
+        <div className="pt-30">
+            <div>{children}</div>
+        </div>
+    );
+};
+
+export default DashBoardLayout;
---
+++ src/app/dashboard/recruiter/page.jsx
@@ -0,0 +1,10 @@
+
+const RecruiterPage = () => {
+    return (
+        <div>
+           <h2>I am a recruiter</h2> 
+        </div>
+    );
+};
+
+export default RecruiterPage;
