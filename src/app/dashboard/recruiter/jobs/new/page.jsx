9407991bbaffc8025b5ec80d1070a000fe86c072
piasmajumdar <piasmajumdar@gmail.com>
2026-06-05 18:06:25 +0530

    Jobs page and new job page path created

---
+++ src/app/dashboard/recruiter/jobs/new/page.jsx
@@ -0,0 +1,10 @@
+
+const NewJob = () => {
+    return (
+        <div>
+            <h2>Create New Job</h2>
+        </div>
+    );
+};
+
+export default NewJob;
---
+++ src/app/dashboard/recruiter/jobs/page.jsx
@@ -0,0 +1,10 @@
+
+const RecruiterJobs = () => {
+    return (
+        <div>
+            <h2>Recruiter/company Manage All jobs</h2>
+        </div>
+    );
+};
+
+export default RecruiterJobs;
