a58ba0db345299c179da41f93f07f30fd6e8a76e
piasmajumdar <piasmajumdar@gmail.com>
2026-06-10 01:44:12 +0530

    Company profile update and edit

--- src/app/dashboard/recruiter/company/page.jsx
+++ src/app/dashboard/recruiter/company/page.jsx
@@ -1,10 +1,19 @@
+import React from 'react';
+import CompanyProfile from './CompanyProfile';
+import { getUserSession } from '@/lib/core/session';
+import { getRecruiterCompany } from '@/lib/api/companies';
 
-const RecruiterCompany = () => {
+const CompanyPage = async () => {
+
+    const user = await getUserSession();
+    const company = await getRecruiterCompany(user?.id);
+    console.log(company)
+
     return (
         <div>
-            <h2>Company Profile</h2>
+            <CompanyProfile recruiter={user} recruiterCompany={company}></CompanyProfile>
         </div>
     );
 };
 
-export default RecruiterCompany;
+export default CompanyPage;
--- src/lib/actions/jobs.js
+++ src/lib/actions/jobs.js
@@ -1,16 +1,21 @@
 'use server'
 
+import { serverMutation } from "../core/server"
+
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
 
-
-export const createJob = async (newJobData) => {
-    const res = await fetch(`${baseUrl}/api/jobs`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(newJobData),
-    });
-
-    return res.json();
+export const createJob = async(newJobData) =>{
+    return serverMutation('/api/jobs', newJobData);
 }
+
+// export const createJob = async (newJobData) => {
+//     const res = await fetch(`${baseUrl}/api/jobs`, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(newJobData),
+//     });
+
+//     return res.json();
+// }
