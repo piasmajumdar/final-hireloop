4ffc68d5161f8f7f62ddbb7d833e0de7ae40657d
piasmajumdar <piasmajumdar@gmail.com>
2026-06-19 10:40:18 +0530

    Added applications page for job seeker

--- src/lib/core/session.js
+++ src/lib/core/session.js
@@ -10,9 +10,13 @@ export const getUserSession = async () => {
     return session?.user || null;
 }
 
-export const requireRole = async(role) =>{
-    const user = await getUserSession();
-    if(user?.role != role){
-        return redirect('/unauthorized')
+export const requireRole = async (role) => {
+    const user = await getUserSession()
+    if (!user) {
+        redirect('/auth/signin')
     }
+    if (user?.role !== role) {
+        redirect('/unauthorized')
+    }
+    return user;
 }
